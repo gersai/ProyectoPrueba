6e4f029eeb8b80741dc92d6fef0d8cb06a4c90a6
Gersai <gersai_ventura@hotmail.com>
2023-08-23 18:53:57 -0600

    tu nombre

---
+++ ProyectoPrueba/MiNombreView.swift
@@ -0,0 +1,20 @@
+//
+//  MiNombreView.swift
+//  ProyectoPrueba
+//
+//  Created by Eduardo Gersai Garcia Ventura on 23/08/23.
+//
+
+import SwiftUI
+
+struct MiNombreView: View {
+    var body: some View {
+        Text("tu Nombre!")
+    }
+}
+
+struct MiNombreView_Previews: PreviewProvider {
+    static var previews: some View {
+        MiNombreView()
+    }
+}
