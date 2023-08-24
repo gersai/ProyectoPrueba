e593948383dc1419163312fcda7b2b178c4fab17
Gersai <gersai_ventura@hotmail.com>
2023-08-23 18:48:01 -0600

    Initial Commit

---
+++ ProyectoPrueba/ProyectoPruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoPruebaApp.swift
+//  ProyectoPrueba
+//
+//  Created by Eduardo Gersai Garcia Ventura on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoPruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
